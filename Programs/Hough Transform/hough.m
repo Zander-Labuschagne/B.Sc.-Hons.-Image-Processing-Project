1e2d7494422aae76e39a0a85370db660a4a2a9e6
Zander Labuschagne <23585137@protonmail.ch>
2017-07-17 08:02:57 +0200

    Added corrections

--- Programs/Hough Transform/hough.m
+++ Programs/Hough Transform/hough.m
@@ -1,7 +1,9 @@
 # Created by Octave 4.2.1, Thu Jul 13 17:19:56 2017 SAST <zander@ArchLinux>
 pkg load image;
 
-#c = binary image;
+c = imread('PTDC0012.JPG');
+c = rgb2gray(c);
+c = imresize(c, 0.2);
 e = edge(c, 'canny');
 
 [x, y] = find(e==1);
@@ -9,7 +11,7 @@ th = 0:179;
 cos_th = cosd(th);
 sin_th = sind(th);
 rtable = floor(x*cos_th + y*sin_th);
-u = unique(rtbale);
+u = unique(rtable);
 N = length(u);
 
 for j = 1 : 180
