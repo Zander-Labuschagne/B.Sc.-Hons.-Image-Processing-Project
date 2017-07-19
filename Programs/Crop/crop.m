05d4d094b2b46fcb992b2aed5dc1423a306a38ab
Zander Labuschagne <23585137@protonmail.ch>
2017-07-19 14:33:17 +0200

    Added pre processing filters

--- Programs/Crop/crop.m
+++ Programs/Crop/crop.m
@@ -1,10 +1,18 @@
-# Created by Octave 4.2.1, Thu Jul 13 17:19:56 2017 SAST <zander@ArchLinux>
-pkg load image;
+% Created by Octave 4.2.1, Thu Jul 13 17:19:56 2017 SAST <zander@ArchLinux>
+%pkg load image;
 
 image = imread('PTDC0012.JPG');
-image = imresize(image, 0.2);
+image = imresize(image, 0.05);
 image_gray = rgb2gray(image);
 image_bin = ~im2bw(image);
+
+laplacian = [0 -1 0; -1 4 -1; 0 -1 0];
+edge_horizon = [-1 -1 -1; 2 2 2; -1 -1 -1];
+edge_vertical = [-1 2 -1; -1 2 -1; -1 2 -1];
+
+image_bin = imfilter(image_bin, laplacian);
+image_bin = imfilter(image_bin, edge_horizon);
+image_bin = imfilter(image_bin, edge_vertical);
 
 sum = 0;
 
