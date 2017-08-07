28d3cdc0f7dad2e6bd487f3796888d81db0349d9
Zander Labuschagne <zander.labuschagne@protonmail.ch>
2017-08-07 08:39:11 +0200

    Morphology idea progressed. Infinity loop fixed, working but line is still drawn straight... I want a curved line along the page

--- Programs/Crop/crop_morph.m
+++ Programs/Crop/crop_morph.m
@@ -7,7 +7,7 @@ clear all;
 
 %% -------------------------Image Preperation------------------------- %%
 
-image = imread('PTDC0012.JPG');
+image = imread('PTDC0073.JPG');
 image_gray = rgb2gray(image);
 image = imresize(image_gray, 0.2);
 image_bin = ~im2bw(image);
@@ -33,9 +33,14 @@ imshow(image_bin_laplace);
 %figure('Name', 'Grayscale Image', 'NumberTitle', 'off');
 %imshow(image_gray);
 
-skew_tolerance = 2;
-line_length_tolerance = 0.2;
+skew_tolerance = 50;
+line_length_tolerance = 0.9;
 page_border_limit = 0.4;
+%tolerance = 0.06;
+%tolerance = 0.061585;
+tolerance = 0.081585;
+
+
 
 %% --------------------------Vertical Lines-------------------------- %%
 line_weight = 0;
@@ -45,8 +50,13 @@ line_index = double(size(image_bin, 2));
 
 i = round(page_border_limit * size(image_bin, 2));
 while i > 0
+     line_weight = 0;
      ii = round(0.5 * size(image_bin, 1) - line_length_tolerance * size(image_bin, 1) / 2);
+     line_index = double(size(image_bin, 2));
+     li = 1;
      while ii <= round(0.5 * size(image_bin, 1) + line_length_tolerance * size(image_bin, 1) / 2)
+        ii
+        skew_detect = 0;
         if image_bin(ii, i) == 1
             line_weight = line_weight + 1;
             line_index(li) = i;
@@ -56,18 +66,16 @@ while i > 0
             while (skew_detect == 1) && (iii <= skew_tolerance)%Check for skew lines
                 if i + iii < size(image_bin, 2) %If skew detection is in bounds of image
                     if image_bin(ii, i + iii) == 1 %If detected skew to right
-                        i = i + iii;
                         skew_detect = 0;
                         line_weight = line_weight + 1;
-                        line_index(li) = i;
+                        line_index(li) = i + iii;
                         li = li + 1;
                     end;
                 elseif i - iii > 0  %If skew detection is in bounds of image
                     if image_bin(ii, i - iii) == 1 %If detected skew to left
-                        i = i - iii;
                         skew_detect = 0;
                         line_weight = line_weight + 1;
-                        line_index(li) = gobjects(size(image_bin, 2));
+                        line_index(li) = i - iii;
                         li = li + 1;
                     end;
                 end;
@@ -76,22 +84,37 @@ while i > 0
         end;
         if skew_detect == 0 %If continuing
             skew_detect = 1;
-        else%If broken line
+        else%If broken line -> Discontinue
             skew_detect = 1;
-            line_weight = 0;
-            
-            line_index(li) = i;
-            li = 1;
+            %line_weight = 0;
+            %line_index = double(size(image_bin, 2));
+            %li = 1;
             ii = round(0.5 * size(image_bin, 1) + line_length_tolerance * size(image_bin, 1) / 2) * 10;%Jump out of ii while loop
         end;
         ii = ii + 1;
     end;%Move vertically through image from line_length_tolerance top to bottom
+    if line_weight / size(image_bin, 1) > tolerance
+        i = 0;
+    end;
     i = i - 1;
 end;%Move horizontally through image from 40% to the right to left
 
-
+fprintf('Start Index: %d\n',  round(page_border_limit * size(image_bin, 2)));
+fprintf('Line Index: %d\n', line_index(1));
 fprintf('Total Line Weight: %d\n', line_weight);
 fprintf('Perc Line Weight: %d\n', line_weight / size(image_bin, 1));
+
+line_index = line_index .* 5;
+
+for iv = round(0.5 * size(image_gray, 1) - line_length_tolerance * size(image_gray, 1) / 2) : round(0.5 * size(image_gray, 1) + line_length_tolerance * size(image_gray, 1) / 2)
+    %image_gray(iv, line_index(mod(iv, 5) + 1)) = 255;
+    for v = 1 : 7
+        image_gray(iv, line_index(mod(iv, 5) + 1) + v) = 255;
+        if line_index(mod(iv, 5) + 1) - v > 0
+            image_gray(iv, line_index(mod(iv, 5) + 1) - v) = 255;
+        end;
+    end;
+end;
 
 
 %% --------------------------Horizontal Lines-------------------------- %%
