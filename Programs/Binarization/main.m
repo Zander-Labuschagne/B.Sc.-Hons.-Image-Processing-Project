17a1d0a2eb120df7d5ac35be37d8512854345bf9
Zander Labuschagne <zander.labuschagne@protonmail.ch>
2017-08-28 10:49:46 +0200

    Experimented with preprocessing noise removal filters for Niblacks algorithm

--- Programs/Binarization/main.m
+++ Programs/Binarization/main.m
@@ -6,7 +6,7 @@ close all;
 clear all;
 
 %% -------------------------Image Preperation------------------------- %%
-image = imread('PTDC0120.JPG');
+image = imread('PTDC0066.JPG');
 image = imresize(image, 0.5);
 image_gray = rgb2gray(image);
 
@@ -27,8 +27,9 @@ image_gray = rgb2gray(image);
      %image_rosenfeld = 
      %% -------------------------Otsu------------------------ %%
      image_bin_otsu = imbinarize(image_gray);
-     image_bin_niblack_15 = niblack(image_gray, 15);
-     image_bin_niblack_9 = niblack(image_gray, 9);
+     %% ------------------------Niblack-----------------------%%
+     %image_bin_niblack_15 = niblack(image_gray, 15);
+     %image_bin_niblack_9 = niblack(image_gray, 9);
      image_bin_niblack_5 = niblack(image_gray, 5);
 %      image_bin_otsu_wiener = imbinarize(image_filter_wiener);
 %      image_bin_otsu_butt = imbinarize(image_filter_butterworth);
@@ -40,26 +41,37 @@ image_gray = rgb2gray(image);
 %% --------------------------Post-Filtering--------------------------- %%
       %Hou maar by original
       %Werk nie
-%     image_bin_filter_wiener = wiener2(image_bin_otsu,[5 5]);
+     %image_bin_filter_wiener5 = wiener2(image_bin_niblack_5,[5 5]);
+     %image_bin_filter_wiener9 = wiener2(image_bin_niblack_5,[9 9]);
+     %image_bin_filter_wiener15 = wiener2(image_bin_niblack_5,[15 15]);
+
 %     %% -----------------Median Low Pass Filter----------------- %%
 %     image_bin_filter_median = medfilt2(image_bin_otsu);
 %     %% ---------------Low Pass Butterworth Filter-------------- %%
-%     image_bin_filter_butterworth = butterworthbpf(image_bin_otsu, 100, 1, 8);
-
+     %image_bin_filter_butterworth = butterworthbpf(image_bin_niblack_5, 100, 1, 8);
 
 %% ------------------------------Finalize------------------------------ %%
 figure('Name', 'Otsu', 'NumberTitle', 'off');
 imagesc(image_bin_otsu);
 colormap('gray');
-figure('Name', 'Niblack 15', 'NumberTitle', 'off');
-imagesc(image_bin_niblack_15);
-colormap('gray');
-figure('Name', 'Niblack 9', 'NumberTitle', 'off');
-imagesc(image_bin_niblack_9);
-colormap('gray');
-figure('Name', 'Niblack 5', 'NumberTitle', 'off');
-imagesc(image_bin_niblack_5);
-colormap('gray');
+% figure('Name', 'Niblack 15', 'NumberTitle', 'off');
+% imagesc(image_bin_niblack_15);
+% colormap('gray');
+% figure('Name', 'Niblack 9', 'NumberTitle', 'off');
+% imagesc(image_bin_niblack_9);
+% colormap('gray');
+ figure('Name', 'Niblack 5', 'NumberTitle', 'off');
+ imagesc(image_bin_niblack_5);
+ colormap('gray');
+% figure('Name', 'Niblack 5 Wiener 5', 'NumberTitle', 'off');
+% imagesc(image_bin_filter_wiener5);
+% colormap('gray');
+% figure('Name', 'Niblack 5 Wiener 15', 'NumberTitle', 'off');
+% imagesc(image_bin_filter_wiener15);
+% colormap('gray');
+% figure('Name', 'Niblack 5 Butterworth', 'NumberTitle', 'off');
+% imagesc(image_bin_filter_butterworth);
+% colormap('gray');
 % figure('Name', 'Butterworth -> Otsu', 'NumberTitle', 'off');
 % imagesc(image_bin_otsu_butt);
 % colormap('gray');
